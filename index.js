d19f3068a9e06d3bffb9f6fa23be5c6035b74f51
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-18 20:03:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(foot){
   // Write your code here!
+
+  if (foot <= 2000) {
+    return 'This one is on me!';
+
+  } else if (foot > 2000 && foot < 2500) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else if (foot >= 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+
+      case 'not as generous':
+        return 'Thank you.'
+
+  break;
+    default:
+      return 'Bye.'
+  }
+
+  
 }
